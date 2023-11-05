7259497a57ee4b571ef9a76eb684d0e20669cd64
Lukas Schneider <54738937+Lukimuc@users.noreply.github.com>
2023-11-05 16:10:36 +0100

    MRTK in Unreal Engine done

---
+++ Plugins/UXToolsExamples-0.12.0-5.0/UXToolsExamples/Intermediate/Build/HoloLens/arm64/UnrealGame/Inc/UXToolsExamples/UxtExamplesInfo.generated.h
@@ -0,0 +1,101 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+class UObject;
+#ifdef UXTOOLSEXAMPLES_UxtExamplesInfo_generated_h
+#error "UxtExamplesInfo.generated.h already included, missing '#pragma once' in UxtExamplesInfo.h"
+#endif
+#define UXTOOLSEXAMPLES_UxtExamplesInfo_generated_h
+
+#define FID_HostProject_Plugins_UXToolsExamples_Source_UXToolsExamples_Private_UxtExamplesInfo_h_20_SPARSE_DATA
+#define FID_HostProject_Plugins_UXToolsExamples_Source_UXToolsExamples_Private_UxtExamplesInfo_h_20_RPC_WRAPPERS \
+ \
+	DECLARE_FUNCTION(execCommitSHA);
+
+
+#define FID_HostProject_Plugins_UXToolsExamples_Source_UXToolsExamples_Private_UxtExamplesInfo_h_20_RPC_WRAPPERS_NO_PURE_DECLS \
+ \
+	DECLARE_FUNCTION(execCommitSHA);
+
+
+#define FID_HostProject_Plugins_UXToolsExamples_Source_UXToolsExamples_Private_UxtExamplesInfo_h_20_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUUxtExamplesInfo(); \
+	friend struct Z_Construct_UClass_UUxtExamplesInfo_Statics; \
+public: \
+	DECLARE_CLASS(UUxtExamplesInfo, UBlueprintFunctionLibrary, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/UXToolsExamples"), NO_API) \
+	DECLARE_SERIALIZER(UUxtExamplesInfo)
+
+
+#define FID_HostProject_Plugins_UXToolsExamples_Source_UXToolsExamples_Private_UxtExamplesInfo_h_20_INCLASS \
+private: \
+	static void StaticRegisterNativesUUxtExamplesInfo(); \
+	friend struct Z_Construct_UClass_UUxtExamplesInfo_Statics; \
+public: \
+	DECLARE_CLASS(UUxtExamplesInfo, UBlueprintFunctionLibrary, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/UXToolsExamples"), NO_API) \
+	DECLARE_SERIALIZER(UUxtExamplesInfo)
+
+
+#define FID_HostProject_Plugins_UXToolsExamples_Source_UXToolsExamples_Private_UxtExamplesInfo_h_20_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UUxtExamplesInfo(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UUxtExamplesInfo) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UUxtExamplesInfo); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UUxtExamplesInfo); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UUxtExamplesInfo(UUxtExamplesInfo&&); \
+	NO_API UUxtExamplesInfo(const UUxtExamplesInfo&); \
+public:
+
+
+#define FID_HostProject_Plugins_UXToolsExamples_Source_UXToolsExamples_Private_UxtExamplesInfo_h_20_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UUxtExamplesInfo(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UUxtExamplesInfo(UUxtExamplesInfo&&); \
+	NO_API UUxtExamplesInfo(const UUxtExamplesInfo&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UUxtExamplesInfo); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UUxtExamplesInfo); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UUxtExamplesInfo)
+
+
+#define FID_HostProject_Plugins_UXToolsExamples_Source_UXToolsExamples_Private_UxtExamplesInfo_h_17_PROLOG
+#define FID_HostProject_Plugins_UXToolsExamples_Source_UXToolsExamples_Private_UxtExamplesInfo_h_20_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_HostProject_Plugins_UXToolsExamples_Source_UXToolsExamples_Private_UxtExamplesInfo_h_20_SPARSE_DATA \
+	FID_HostProject_Plugins_UXToolsExamples_Source_UXToolsExamples_Private_UxtExamplesInfo_h_20_RPC_WRAPPERS \
+	FID_HostProject_Plugins_UXToolsExamples_Source_UXToolsExamples_Private_UxtExamplesInfo_h_20_INCLASS \
+	FID_HostProject_Plugins_UXToolsExamples_Source_UXToolsExamples_Private_UxtExamplesInfo_h_20_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_HostProject_Plugins_UXToolsExamples_Source_UXToolsExamples_Private_UxtExamplesInfo_h_20_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_HostProject_Plugins_UXToolsExamples_Source_UXToolsExamples_Private_UxtExamplesInfo_h_20_SPARSE_DATA \
+	FID_HostProject_Plugins_UXToolsExamples_Source_UXToolsExamples_Private_UxtExamplesInfo_h_20_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_HostProject_Plugins_UXToolsExamples_Source_UXToolsExamples_Private_UxtExamplesInfo_h_20_INCLASS_NO_PURE_DECLS \
+	FID_HostProject_Plugins_UXToolsExamples_Source_UXToolsExamples_Private_UxtExamplesInfo_h_20_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> UXTOOLSEXAMPLES_API UClass* StaticClass<class UUxtExamplesInfo>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_HostProject_Plugins_UXToolsExamples_Source_UXToolsExamples_Private_UxtExamplesInfo_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Plugins/UXToolsExamples-0.12.0-5.0/UXToolsExamples/Intermediate/Build/Win64/UnrealGame/Inc/UXToolsExamples/UXToolsExamples.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeUXToolsExamples_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_UXToolsExamples;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_UXToolsExamples()
+	{
+		if (!Z_Registration_Info_UPackage__Script_UXToolsExamples.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/UXToolsExamples",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x68752B2F,
+				0xA3C01287,
+				METADATA_PARAMS(nullptr, 0)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_UXToolsExamples.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_UXToolsExamples.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_UXToolsExamples(Z_Construct_UPackage__Script_UXToolsExamples, TEXT("/Script/UXToolsExamples"), Z_Registration_Info_UPackage__Script_UXToolsExamples, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x68752B2F, 0xA3C01287));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
